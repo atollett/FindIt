8886d770914a873e023fb552764f0b71563a1afa
atollett <a.t.tollett@gmail.com>
2020-07-25 14:00:31 -0700

    got googlebooks working, still working on openlibrary, quotes and log in

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
 
 queryURL = "https://www.googleapis.com/books/v1/volumes?q="
-//queryURL = "https://www.goodreads.com/book/harrypotter.FORMAT"
 
-$("#button").click(function(){
+$("#buttonTitle").click(function(){
   // var $bookQuery = $('<input>');
   // $bookQuery.val();
   // document.body.append($bookQuery);
 
-  var bookQuery = $( "#bookQuery" ).val();
+  //var bookQuery = $( "#titleQuery" ).val();
   //queryURL += bookQuery;
-
-  queryURL = "http://openlibrary.org/search.json?q=elantris"
-
+  //queryURL = "http://openlibrary.org/search.json?q=sciencefiction"
+  //queryURL = "http://ia700401.us.archive.org/fulltext/inside.php?item_id=designevaluation25clin&doc=designevaluation25clin&path=/28/items/designevaluation25clin&q=%22library%20science%22&callback=reply";
+  queryURL = "https://api.paperquotes.com/quotes?tags=love,life&language=en";
+  
   var priceArray = [];
+
     
     $.ajax({
         //crossDomain: true,
@@ -56,8 +57,8 @@ $("#button").click(function(){
         //   }
         // }
 
-        // priceArray.sort();
-        // $("#appendPrice").append(priceArray);
+        //  priceArray.sort();
+        //  $("#appendPrice").append(priceArray.toString());
 
         // // console.log(response.items[0].volumeInfo.authors[0]);
         // // console.log(response.items[0].volumeInfo.description);
@@ -66,3 +67,27 @@ $("#button").click(function(){
       });
 });
 
+
+
+$("#buttonAuthor").click(function(){
+  var authorQuery = $( "#authorQuery" ).val();
+  queryURL += authorQuery;
+
+  var bookArray = [];
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+
+    for(var i = 0; i < 10; ++i) {
+      bookArray.push(response.items[i].volumeInfo.title);
+    }
+    console.log(bookArray.toString());
+    console.log(response);
+
+    $("#appendBookList").append(bookArray.toString());
+    
+
+  });
+});
