c90f2fc4f7b06c600eafc17294c28ee54d6b7538
atollett <a.t.tollett@gmail.com>
2020-07-30 19:12:57 -0700

    updates musixmatch and omdb

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
+//musixmatch
+var apiKEY = "340a1b7db75a96676f20c4b616e09971";
+//queryURL = "https://api.musixmatch.com/ws/1.1/";
 
 
-queryURL = "https://www.googleapis.com/books/v1/volumes?q="
+//queryURL = "https://www.googleapis.com/books/v1/volumes?q="
 
 $("#buttonTitle").click(function(){
   // var $bookQuery = $('<input>');
   // $bookQuery.val();
   // document.body.append($bookQuery);
+  
+  var musicQuery = $( "#titleQuery" ).val();
+  queryURL = "https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=" + musicQuery + "&page_size=3&page=1&s_track_rating=desc&apikey=" + apiKEY;
+
+  
+  //queryURL += musicQuery;
+  
 
   //var bookQuery = $( "#titleQuery" ).val();
   //queryURL += bookQuery;
   //queryURL = "http://openlibrary.org/search.json?q=sciencefiction"
   //queryURL = "http://ia700401.us.archive.org/fulltext/inside.php?item_id=designevaluation25clin&doc=designevaluation25clin&path=/28/items/designevaluation25clin&q=%22library%20science%22&callback=reply";
-  queryURL = "https://api.paperquotes.com/quotes?tags=love,life&language=en";
+  //queryURL = "https://api.paperquotes.com/quotes/?tags=love,life&lang=en";
   
   var priceArray = [];
 
-    
+  //var movie = "Twilight";
+  //var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+
+  //var queryURL = "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10";
+
+
     $.ajax({
-        //crossDomain: true,
-        // headers: {
-        //     "Access-Control-Allow-Origin": "*",
-        //     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-        // },
+        crossDomain: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+        },
         url: queryURL,
         method: "GET"
       }).then(function(response) {
 
         console.log(response);
 
+        console.log(response.message.body.track_list.track.track_name);
 
+        //$("#movieTitle").append(response.Title);
+
+        //$("#thumbnailBook").attr("src", response.items[0].volumeInfo.imageLinks.thumbnail);
 
 
 
@@ -36,7 +55,7 @@ $("#buttonTitle").click(function(){
         // $("#appendAuthor").append(response.items[0].volumeInfo.authors[0]);
         // $("#appendDescription").append(response.items[0].volumeInfo.description);
         // $("#appendTitle").append(response.items[0].volumeInfo.title);
-       
+        // add cover
 
         // for (var i = 0; i < 10; ++i) {
         //   //var price = response.items[0].saleInfo.retailPrice.amount;
@@ -66,6 +85,9 @@ $("#buttonTitle").click(function(){
         // console.log(response);
       });
 });
+
+
+
 
 
 
